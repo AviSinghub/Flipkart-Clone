cb5cbf06acfcb94e4a5de78102c3d3aca9e68662
Avinash <crazydinclub@gmail.com>
2023-12-13 10:03:51 +0530

    Responsive design fixed to navbar

--- client/src/components/headers/Headers.jsx
+++ client/src/components/headers/Headers.jsx
@@ -1,4 +1,7 @@
-import { AppBar, Toolbar, styled, Box, Typography} from '@mui/material';
+import { useState } from 'react';
+import { AppBar, Toolbar, styled, Box, IconButton, Drawer, List,ListItem, Typography} from '@mui/material';
+
+import {Menu} from '@mui/icons-material';
 
 // components 
 import Search from './Search';
@@ -32,19 +35,58 @@ const PlusImage = styled('img')(
     }
 );
 
-const CustomButtonWrapper = styled(Box)`
-    margin: 0 5% 0 auto;
-`;
+const CustomButtonWrapper = styled(Box)(({ theme }) => ({
+    margin: '0 5% 0 auto',
+    [theme.breakpoints.down('md')]: {
+        display: 'none'
+    }
+}));
+
+const MenuButton = styled(IconButton)(({ theme }) => ({
+    display: 'none',
+    [theme.breakpoints.down('md')]: {
+        display:'block'
+    }
+    
+}))
 
 const Header = () => {
 
     const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
     const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
 
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+
+    const list = () => (
+        <Box style={{ width: 200}} onClick={handleClose}>
+            <List>
+                <ListItem button>
+                    <CustomButtons/>
+                </ListItem>
+            </List>
+        </Box>
+    );
+
     return ( 
 
         <StyledHeader>
             <Toolbar style={{minHeight: 55}} >
+                <MenuButton color="inherit" onClick={handleOpen} >
+                    <Menu/>
+                </MenuButton>
+
+                <Drawer open={open} onClose={handleClose}>
+                    {list()}
+                </Drawer>
+                 
                 <Component to='/'>
                     <img src= {logoURL} alt= 'logo' style={{ width: 75 }} /> 
                     <Box style={{ display: 'flex'}}>
