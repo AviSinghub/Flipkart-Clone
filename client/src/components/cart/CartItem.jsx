a03e0d2c5df8a39d4ddff2c8fd92f9b0b08a47aa
Avinash <crazydinclub@gmail.com>
2023-12-29 13:48:15 +0530

    Cart feature updated w/group

--- client/src/components/cart/CartItem.jsx
+++ client/src/components/cart/CartItem.jsx
@@ -1,4 +1,8 @@
-import { Box, Typography, styled } from "@mui/material";
+import { Box, Button, Typography, styled } from "@mui/material";
+
+import { addEllipsis } from "../../utils/common-utils";
+
+import ButtonGroup from "./ButtonGroup";
 
 // component 
 const Component = styled(Box)`
@@ -6,17 +10,47 @@ const Component = styled(Box)`
     display: flex;
 `;
 
+const LeftComponent = styled(Box)`
+    margin: 20px;
+    display: flex;
+    flex-direction: column;
+`;
+
+const Smalltext = styled(Typography)`
+    color: #878787;
+    font-size: 14px;
+    margin-top: 10px;
+`;
+const Remove = styled(Button)`
+    margin-top: 20px;
+    font-size: 16px;
+    color: #000;
+    font-weight: 600;
+`;
+
 const CartItem = ({item}) => {
+    const fassured = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png';
+
     return (
         <Component>
             {/* LHS  */}
-            <Box>
-                <img src={item.url} alt="product"/>
-            </Box>
+            <LeftComponent>
+                <img src={item.url} alt="product" style={{ height: 110, width:110}}/>
+                <ButtonGroup/>
+            </LeftComponent>
 
             {/* RHS  */}
-            <Box>
-                <Typography>{item.title.longTitle}</Typography>
+            <Box style={{ margin: 20}}>
+                <Typography>{addEllipsis(item.title.longTitle)}</Typography>
+                <Smalltext>Seller:Cloudtail
+                    <Box component="span" ><img src={fassured} alt="f_assured" style={{ width: 50, marginLeft: 10}} /></Box>
+                </Smalltext>
+                <Typography style={{ margin: '20px 0'}} >
+                    <Box component="span" style={{fontWeight:600, fontSize: 18}} >₹{item.price.cost}</Box>&nbsp;&nbsp;&nbsp;
+                    <Box component="span" style={{color:'#878787'}} ><strike>₹{item.price.mrp}</strike></Box>&nbsp;&nbsp;&nbsp;
+                    <Box component="span" style={{color:'#388E3C'}}>{item.price.discount}Off</Box>
+                </Typography>
+                <Remove>Remove</Remove>
             </Box>
         </Component>
     );
